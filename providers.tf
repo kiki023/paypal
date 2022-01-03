6df315a14781e7bdb547b14704454b83d6a72824
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-03 16:16:25 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,15 +2,15 @@
 # Provider Configuration
 #
 
-provider "aws" {
-  region  = "us-west-1"
+# provider "aws" {
+#  region  = "us-west-1"
 }
 
 # Using these data sources allows the configuration to be
 # generic for any region.
 data "aws_region" "current" {}
 
-data "aws_availability_zones" "available" {}
+# data "aws_availability_zones" "available" {}
 
 # Not required: currently used in conjuction with using
 # icanhazip.com to determine local workstation external IP
