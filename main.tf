6c3805673e0e63cd3017b40c4f57c349131e673a
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-03 19:38:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,7 @@ terraform {
 
 provider "aws" {
         region = "us-west-1"
-        access_key = var.AWS_ACCESS_KEY_ID
-	secret_key = var.AWS_SECRET_ACCESS_KEY
+        shared_credentials_file = "~/eksadmin/.aws/credentials"
 }
 
 resource "aws_instance" "ec2" {
