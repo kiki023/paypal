a1cb727c0a517cc85fd8fbfc498822b3f9dab56e
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-03 16:02:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,13 +32,5 @@ resource "aws_instance" "ec2" {
     Envrionment = "production"
   }
 }
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 9080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+
 
