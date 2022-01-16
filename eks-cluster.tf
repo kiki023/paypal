f1858d90ed98a99ea6b199c17ab7de55a6f27994
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-16 15:49:09 +0100

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -37,12 +37,6 @@ resource "aws_security_group" "demo-cluster" {
   name        = "terraform-eks-demo-cluster"
   description = "Cluster communication with worker nodes"
   vpc_id      = aws_vpc.demo.id
-  
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -60,10 +54,10 @@ resource "aws_security_group" "demo-cluster" {
 resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
   cidr_blocks       = [local.workstation-external-cidr]
   description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443 
+  from_port         = 80 
   protocol          = "tcp"
   security_group_id = aws_security_group.demo-cluster.id
-  to_port           = 443 
+  to_port           = 80 
   type              = "ingress"
 }
 
