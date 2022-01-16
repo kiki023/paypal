6ea0af110cf9aeca5583ed74bfdea8c1e250bd7e
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-16 16:31:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,11 +39,11 @@ data "kubectl_file_documents" "argocd" {
     content = file("./install.yaml")
 }
 
-resource "kubectl_manifest" "namespace" {
-    count     = length(data.kubectl_file_documents.namespace.documents)
-    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
-    override_namespace = "argocd"
-}
+#resource "kubectl_manifest" "namespace" {
+#    count     = length(data.kubectl_file_documents.namespace.documents)
+#    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
+#    override_namespace = "argocd"
+#}
 
 resource "kubectl_manifest" "argocd" {
     depends_on = [
