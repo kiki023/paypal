f348c10a461493187403cc5b4829173866652996
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-16 08:38:47 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,18 @@
 #
 # Provider Configuration
 #
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.demo-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.demo-cluster.certificate_authority.0.data)
+
+  exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.id]
+      command     = "aws"
+    }
+
+  load_config_file       = false
+}
 
 # provider "aws" {
 #  region  = "us-west-1"
