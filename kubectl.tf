2bbbf8d089680ae91aee981003bd87610f5eb317
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-16 10:14:05 +0100

    Update kubectl.tf

--- kubectl.tf
+++ kubectl.tf
@@ -10,13 +10,6 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.demo-cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.demo-cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.demo-cluster.token
-  load_config_file       = false
-}
-
 data "kubectl_file_documents" "namespace" {
     content = file("../namespace.yaml")
 } 
