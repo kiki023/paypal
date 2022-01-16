344076bcbb8d38d0b91245edb25d4102dafa3fd0
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-16 08:50:11 +0100

    Create kubectl.tf

---
+++ kubectl.tf
@@ -0,0 +1,34 @@
+
+
+
+
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.demo-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.demo-cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.demo-cluster.token
+  load_config_file       = false
+}
+
+data "kubectl_file_documents" "namespace" {
+    content = file("../namespace.yaml")
+} 
+
+data "kubectl_file_documents" "argocd" {
+    content = file("../argocd.yaml")
+}
+
+resource "kubectl_manifest" "namespace" {
+    count     = length(data.kubectl_file_documents.namespace.documents)
+    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
+    override_namespace = "argocd"
+}
+
+resource "kubectl_manifest" "argocd" {
+    depends_on = [
+      kubectl_manifest.namespace,
+    ]
+    count     = length(data.kubectl_file_documents.argocd.documents)
+    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
+    override_namespace = "argocd"
+}
