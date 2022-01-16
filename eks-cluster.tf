547ee3153d45d6f2983692a3cd2b961c3ca1be6a
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-16 15:51:35 +0100

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -54,7 +54,17 @@ resource "aws_security_group" "demo-cluster" {
 resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
   cidr_blocks       = [local.workstation-external-cidr]
   description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 80 
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.demo-cluster.id
+  to_port           = 443 
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
+  cidr_blocks       = [local.workstation-external-cidr]
+  description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 80
   protocol          = "tcp"
   security_group_id = aws_security_group.demo-cluster.id
   to_port           = 80 
