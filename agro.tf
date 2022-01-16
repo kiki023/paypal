f893232bb191f9b7b7fd3d3e41c0a2565b8b2bce
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-01-16 09:03:00 +0100

    Create agro.tf

---
+++ agro.tf
@@ -0,0 +1,12 @@
+data "kubectl_file_documents" "nginx2" {
+    content = file("../nginx2.yaml")
+}
+
+resource "kubectl_manifest" "nginx2" {
+    depends_on = [
+      kubectl_manifest.argocd,
+    ]
+    count     = length(data.kubectl_file_documents.nginx2.documents)
+    yaml_body = element(data.kubectl_file_documents.nginx2.documents, count.index)
+    override_namespace = "argocd"
+}
