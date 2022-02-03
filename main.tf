6aa80b59f98388ec272e7efa81d4d5a7e324d532
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-03 14:37:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,37 +19,6 @@ terraform {
 
 provider "aws" {
         region = "us-west-1"
-        shared_credentials_file = "~/eksadmin/.aws/credentials"
-}
-
-resource "aws_instance" "ec2" {
-    ami = "ami-054965c6cd7c6e462"
-    instance_type = "t2.medium"
-    tags = {
-    Name = "testingDotPay"
-    Envrionment = "production"
-  }
-}
-
-data "kubectl_file_documents" "namespace" {
-    content = file("./namespace.yaml")
-} 
-
-data "kubectl_file_documents" "argocd" {
-    content = file("./install.yaml")
-}
-
-resource "kubectl_manifest" "namespace" {
-    count     = length(data.kubectl_file_documents.namespace.documents)
-    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
-    override_namespace = "argocd"
-}
-
-resource "kubectl_manifest" "argocd" {
-    depends_on = [
-     kubectl_manifest.namespace,
-    ]
-    count     = length(data.kubectl_file_documents.argocd.documents)
-    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
-    override_namespace = "argocd"
+        aws_access_key:  ${{ secrets.AWS_ACCESS_KEY_ID }}
+        aws_secret_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
