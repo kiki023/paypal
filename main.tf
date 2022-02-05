3bfcc868e5ee3518a9e17c15526499642d004992
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-05 13:15:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,19 @@ terraform {
     //key            = "terraform.tfstate"
     //region         = "us-west-1"
     //dynamodb_table = "camchi1"
-  }
+  
   
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"   
+    }
+  }
+  cloud {
+    organization = "LoveChi"
+
+    workspaces {
+      name = "gh-actions-dotpay"
     }
   }
 }
