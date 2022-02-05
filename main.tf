b4371b9f124bc81fc5b70435a57468314b384c66
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-05 12:52:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,14 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-west-1"
     dynamodb_table = "camchi1"
-
+  }
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    
+      version = ">= 1.7.0" 
+      
+    }
+  }  
   cloud {
     organization = "LoveChi"
 
